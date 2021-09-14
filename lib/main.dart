170714b002571f6c04321bdbbbdc1d276cf8a1dd
Felipe Sales <soufeliposales@gmail.com>
2021-09-13 22:05:55 -0300

    adding and configuring for the firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 
 import 'modules/splash/splash_module.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     const MyApp(),
   );
