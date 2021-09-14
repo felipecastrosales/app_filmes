b61988b3ee057bbc624be0c5cbbc5be6ea565582
Felipe Sales <soufeliposales@gmail.com>
2021-09-13 21:13:10 -0300

    configurating for a getX and starting the structuring

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
+
+import 'modules/splash/splash_module.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'App Filmes',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      getPages: [
+        ...SplashModule().routers,
+      ],
     );
   }
 }
---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import './splash_controller.dart';
+
+class SplashPage extends GetView<SplashController> {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SplashPage'),
+      ),
+      body: Container(),
+    );
+  }
+}
