317b14684f9a335e26151db5fd3d2fecc3bf328f
Felipe Sales <soufeliposales@gmail.com>
2021-09-17 10:37:35 -0300

    updating genres, adding rest_client for a baseUrl and services for genres, creating and up the modules

--- lib/application/bindings/application_bindings.dart
+++ lib/application/bindings/application_bindings.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 
+import 'package:app_filmes/application/rest_client/rest_client.dart';
 import 'package:app_filmes/application/auth/auth_service.dart';
 import 'package:app_filmes/repositories/login/login_repository_impl.dart';
 import 'package:app_filmes/repositories/login/login_repository.dart';
@@ -9,6 +10,9 @@ import 'package:app_filmes/services/login/login_service_impl.dart';
 class ApplicationBindings implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut(
+      () => RestClient(),
+    );
     Get.lazyPut<LoginRepository>(
       () => LoginRepositoryImpl(),
       fenix: true,
