02ce51869a7b132ef97f614805fe856697f81a00
Felipe Sales <soufeliposales@gmail.com>
2021-09-21 21:59:04 -0300

    adding the flow for favorites page and adding this module

--- lib/modules/favorites/favorites_page.dart
+++ lib/modules/favorites/favorites_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class FavoritesPage extends StatelessWidget {
+import 'package:get/get.dart';
+
+import 'package:app_filmes/application/ui/widgets/movie_card.dart';
+import 'favorites_controller.dart';
+
+class FavoritesPage extends GetView<FavoritesController> {
   const FavoritesPage({Key? key}) : super(key: key);
 
   @override
@@ -10,7 +15,25 @@ class FavoritesPage extends StatelessWidget {
         title: const Text('Favorites'),
         automaticallyImplyLeading: false,
       ),
-      body: Container(),
+      body: Obx(() {
+        return SingleChildScrollView(
+          child: SizedBox(
+            width: Get.width,
+            child: Wrap(
+              alignment: WrapAlignment.spaceAround,
+              children: controller.movies
+                  .map(
+                    (m) => MovieCard(
+                      movie: m,
+                      favoriteCallback: () =>
+                          controller.removeFavorite(movie: m),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
